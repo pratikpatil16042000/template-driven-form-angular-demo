861ae04487acb9c529076bb4d9bb018f9de0ca13
pratikpatil16042000 <123560463+pratikpatil16042000@users.noreply.github.com>
2024-04-14 14:18:56 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'template-driven-form-demo';
+  topics = ['Angular', 'React', 'Vue'];
+  topicHasError=true;
+  submitted=false;
+
+  userModel = new User('Pratik','',99999999999,'default','morning',true);
+
+  validateTopic(value :any){
+    if(value==='default'){
+      this.topicHasError=true;
+    }else{
+      this.topicHasError=false;
+    }
+  }
+
+  onSubmit(){
+    this.submitted=true;
+    console.log(this.userModel);
+  }
 }
